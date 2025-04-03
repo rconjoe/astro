fb6a5869af3cb508dc15dddf522e896e7314e69d
rconjoe <root@trog.codes>
2025-04-02 20:04:11 -0700

    bumps

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,14 +6,15 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cs" },
-  { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.svelte" },
+  -- { import = "astrocommunity.pack.svelte" },
   -- import/override with your plugins folder
 }
