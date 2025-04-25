6a0e07508f705104d02d744995794edeb34c8bb1
rconjoe <root@trog.codes>
2025-04-24 23:14:47 -0700

    add go stuff

--- lua/community.lua
+++ lua/community.lua
@@ -15,6 +15,7 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.pack.svelte" },
   -- import/override with your plugins folder
 }
